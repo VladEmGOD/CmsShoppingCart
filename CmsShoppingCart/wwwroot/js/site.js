e371c1ec3016d141ae149224ccb465fa3ba8c8e4
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-01-21 22:48:16 +0200

    Added protection for creating a product, now you need to select a category

--- CmsShoppingCart/wwwroot/js/site.js
+++ CmsShoppingCart/wwwroot/js/site.js
@@ -11,3 +11,18 @@
         setTimeout(() => { $("div.alert.notification").fadeOut() }, 2000);
     }
 })
+
+function readURL(input)
+{
+    if (input.files && input.files[0])
+    {
+        let reader = new FileReader();
+
+        reader.onload = (e) =>
+        {
+            $("img#imgpreview").attr("src", e.target.result).width(200).height(200);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
