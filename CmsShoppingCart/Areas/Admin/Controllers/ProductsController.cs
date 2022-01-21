5b601e6ac82750063f4dec77b2355b9a1da08a56
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-01-21 23:38:42 +0200

    Added create product functionality and fix some bugs

--- CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
@@ -1,9 +1,12 @@
 ﻿using CmsShoppingCart.Infrastucture;
+using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,9 +17,12 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
     {
         private readonly CmsShoppingCartContext _context;
 
-        public ProductsController(CmsShoppingCartContext context)
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public ProductsController(CmsShoppingCartContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
         //Get: /admin/Products/
@@ -31,6 +37,51 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             ViewBag.CategoryId = new SelectList(_context.Categories.OrderBy(x => x.Sorting),"Id","Name");
 
             return View();
+        }
+
+        //POST admin/products/create/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            ViewBag.CategoryId = new SelectList(_context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
+
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.ToLower().Replace(" ", "-");
+
+                var slug = await _context.Products.FirstOrDefaultAsync(x => x.Slug == product.Slug);
+
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The product already exists!");
+                    return View(product);
+                }
+
+                string imageName = "noimage.png";
+
+                if (product.ImageUpload != null) 
+                {
+                    string uploadDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+                    imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                    string filePAth = Path.Combine(uploadDir, imageName);
+                    FileStream fs = new FileStream(filePAth, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                }
+
+                product.Image = imageName;
+
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+
+                TempData["Success"] = "The product has been created";
+
+                return RedirectToAction("index");
+            }
+
+            return View(product);
+
         }
     }
 }
