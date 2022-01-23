84740b75ebcbdd5bcda430af935375a1df060443
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-01-23 17:48:31 +0200

    create products controller for users

---
+++ CmsShoppingCart/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using CmsShoppingCart.Infrastucture;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly CmsShoppingCartContext _context;
+
+        public ProductsController(CmsShoppingCartContext context)
+        {
+            _context = context;
+        }
+
+        //GET /products
+        public async Task<IActionResult> Index(int p = 1)
+        {
+            int pageSize = 6;
+
+            var products = _context.Products.OrderByDescending(x => x.Id)
+                .Take(pageSize);
+
+            ViewBag.PageNumber = p;
+            ViewBag.PageRange = pageSize;
+            ViewBag.TotlaPages = (int)Math.Ceiling((decimal)_context.Products.Count() / pageSize);
+
+            return View(await products.ToListAsync());
+        }
+    }
+}
