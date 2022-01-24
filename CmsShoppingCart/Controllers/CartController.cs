9cea1a246eaec4e13d71feda98dc22c2ce5a0705
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-01-24 16:37:01 +0200

    Create sessions for CartController

---
+++ CmsShoppingCart/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using CmsShoppingCart.Infrastucture;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly CmsShoppingCartContext _context;
+
+        public CartController(CmsShoppingCartContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
