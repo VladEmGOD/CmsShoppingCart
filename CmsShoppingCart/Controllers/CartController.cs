de977c1b0cb8e3c3cd60541a8384d15189d33b18
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-02-01 22:06:37 +0200

    remove async word from Decreace

--- CmsShoppingCart/Controllers/CartController.cs
+++ CmsShoppingCart/Controllers/CartController.cs
@@ -55,7 +55,7 @@ namespace CmsShoppingCart.Controllers
             return RedirectToAction("Index");
         }
 
-        public async Task<IActionResult> Decrease(int id)
+        public Task<IActionResult> Decrease(int id)
         {
             List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
 
