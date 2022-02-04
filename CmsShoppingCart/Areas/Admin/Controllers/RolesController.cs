2360f684d3abda823d832bdf126950f8fb878f47
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-02-04 16:47:11 +0200

    Added editing roles for users

--- CmsShoppingCart/Areas/Admin/Controllers/RolesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/RolesController.cs
@@ -15,6 +15,8 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
         private readonly RoleManager<IdentityRole> roleManager;
         private readonly UserManager<AppUser> userManager;
 
+        public object AppUSer { get; private set; }
+
         public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
         {
             this.roleManager = roleManager;
@@ -22,24 +24,24 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
         }
 
         //GET /admin/roles
-        public IActionResult Index() => View(roleManager.Roles); 
-        
+        public IActionResult Index() => View(roleManager.Roles);
+
         //GET /admin/roles/create
         public IActionResult Create() => View();
 
         //POST /admin/roles/create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([MinLength(2), Required] string name) 
+        public async Task<IActionResult> Create([MinLength(2), Required] string name)
         {
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
                 IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
 
-                if (result.Succeeded) 
+                if (result.Succeeded)
                 {
                     TempData["Success"] = "The Role has been created successfully";
-                    return Redirect("Index"); 
+                    return Redirect("Index");
                 }
                 else foreach (IdentityError error in result.Errors) ModelState.AddModelError("", error.Description);
             }
@@ -47,5 +49,44 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             ModelState.AddModelError("", "Minimum Length is 2");
             return View();
         }
+        //GET /admin/roles/edit/S
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+
+            List<AppUser> members = new List<AppUser>();
+            List<AppUser> nonMembers = new List<AppUser>();
+
+            foreach (AppUser user in userManager.Users)
+            {
+                var list = await userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+                list.Add(user);
+            }
+
+            return View(new RoleEdit { Role = role, Members = members, NonMembers = nonMembers });
+        }
+
+        //Post  /admin/roles/edit/S
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(RoleEdit roleEdit)
+        {
+            IdentityResult result;
+
+            foreach (string userId in roleEdit.AddIds ?? new string[] { }) 
+            {
+                AppUser user = await userManager.FindByIdAsync(userId);
+                result = await userManager.AddToRoleAsync(user, roleEdit.RoleName);
+            }
+
+            foreach (string userId in roleEdit.DeleteIds ?? new string[] { })
+            {
+                AppUser user = await userManager.FindByIdAsync(userId);
+                result = await userManager.RemoveFromRoleAsync(user, roleEdit.RoleName);
+            }
+
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+
     }
 }
